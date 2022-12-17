5f85d32154663c04c462bb58ab4bde13bae05619
Peal1997 <hasanpeal65@gmail.com>
2022-12-16 20:48:15 -0600

    form submit

---
+++ function.js
@@ -0,0 +1,4 @@
+const alert = (type , msg) =>{
+    return `<span class = "alert alert-${type} d-flex justify-content-between">${msg}<button class = "btn-close" data-bs-dismiss = "alert"></button></span>`
+}
+
